9107f41171e551c3b270d65ead286d5619557e26
Guilherme Pace <pace.gui@gmail.com>
2021-01-21 20:02:19 -0600

    complete phase 2

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
-// Imports
+// External Imports
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
+// Local Imports
 const routes = require('./routes');
 
 const { environment } = require('./config');
@@ -13,12 +16,12 @@ const isProduction = environment === 'production';
 // Setup express app
 const app = express();
 
-// Setup use of middleware
+// Middleware
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 if (!isProduction) {
-    //Enable cors only in development environment
+    // Enable cors only in development environment
     app.use(cors());
 };
 app.use(helmet({
@@ -34,5 +37,34 @@ app.use(
     })
 )
 app.use(routes);
+// Resource Not Found Error-Handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+// Sequelize Error-Handler
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+// Error Formatter Error-Handler
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
+
 
 module.exports = app;
