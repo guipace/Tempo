6363aa153a10ce6b91b8924e63aeaf595a536f20
Guilherme Pace <pace.gui@gmail.com>
2021-01-22 12:56:10 -0600

    complete phase 3

---
+++ backend/routes/api/index.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
+
+// // GET /api/set-token-cookie
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'DemoUser'
+//         },
+//     })
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+// // GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get('/restore-user', restoreUser, (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+
+module.exports = router;
