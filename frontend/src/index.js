6a67cd7089f44e9210e5be3170545e377f5ddd74
Guilherme Pace <pace.gui@gmail.com>
2021-01-22 20:35:33 -0600

    complete form page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, fetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
   window.csrfFetch = fetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 const Root = () => {
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 // TODO: INCLUDE REDUCERS TO COMBINE
 const rootReducer = combineReducers({
-    
+    session: sessionReducer,
 });
 
 let enhancer;
