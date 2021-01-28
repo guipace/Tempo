dc422d2eb342133282adac437baf27632c449506
Guilherme Pace <pace.gui@gmail.com>
2021-01-27 21:26:13 -0600

    finish branch

--- frontend/src/components/TrackPage/TrackMainInfo.js
+++ frontend/src/components/TrackPage/TrackMainInfo.js
@@ -10,12 +10,12 @@ const TrackMainInfo = ({track, user}) => {
             <div className='flex flex-row flex-nowrap p-10'>
                 <div className='w-50 flex-initial'><img src={track.imageUrl} alt='Track' className='shadow-2xl rounded'></img></div>
                 <div className='flex-grow flex flex-col pl-5 pt-5 pr-5'>
-                    <div className='flex flex-row justify-between pb-5'>
+                    <div className='flex flex-row justify-between pb-3'>
                         <div className='text-silver'>{user.username}</div>
                         <div className='text-silver'>Uploaded: {uploadDate}</div>
                     </div>
-                    <div className='text-silver pb-5'> PLACEHOLDER</div>
-                    <div className='text-4xl text-mandarin pb-5'>{track.title} RÜFÜS DU SOL [DJ Set] - Robot Heart - Burning Man 2019</div>
+                    <div className='text-silver pb-3'> PLACEHOLDER</div>
+                    <div className='text-4xl text-mandarin pb-3'>{track.title} RÜFÜS DU SOL [DJ Set] - Robot Heart - Burning Man 2019</div>
                     <TrackWaveForm track={track}/>
                 </div>
             </div>
